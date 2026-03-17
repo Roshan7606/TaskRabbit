83c2953786827b8c692b9ec9ebe4f0bde98cbb89
Anurag Jagani <anuragjagani.dev@gmail.com>
2026-03-18 01:31:52 +0530

    Revert "icon front page batava madya"
    This reverts commit a0e6b116abc66078a511a17d0a7475e26757dfed.

--- application/views/index.php
+++ application/views/index.php
@@ -6,7 +6,6 @@
         <meta name="viewport" content="width=device-width,initial-scale=1">
         <meta name="keywords" content="#">
         <meta name="description" content="#">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
         <title>TaskRabbit</title>
         <?php
         $this->load->view("CSS");
